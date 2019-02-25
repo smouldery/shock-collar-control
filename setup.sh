5034873da40d802b9e766774fbe87580b1e581f0
smouldery <sophie.e.mulder@gmail.com>
2019-02-25 21:56:21 +1100

    fix to code that broke on 0.6 release.
    C Code swapped from using pigpiod library to using direct sysfs updating
    testing indicates this should fix the issue. update to setup.sh to remove unnnecacary dependancies.

--- setup.sh
+++ setup.sh
@@ -9,9 +9,8 @@ echo "installing the required SYSTEM packages (python packages are done in the n
 echo "we use python3 to run the control script for the transmitter, and talk to the discord bot."
 echo "we use python3-pip to install the python modules for the python script."
 echo "installing these two now"
-apt-get install -y python3 python3-pip pigpio
-echo "done!"
-echo "now we install two python modules - gpiozero controls the gpio pins + transmitter, and discord talks to the discord bot."
+apt-get install -y python3 python3-pip
+echo "done! now we install the discord module which talks to the discord bot."
 echo "we use the pyton installer we installed above to do this with 'pip3 install <packagename>"
 pip3 install discord
 echo "discord module installed!"
--- transmitter.c
+++ transmitter.c
@@ -1,21 +1,42 @@
-// gcc -o transmitter -lpigpio -lpthread -lm transmitter.c
+// gcc -o transmitter -lpigpio -lpthread -lm transmitter.c\\
 
-/* tell the compiler to use pigpio */
-#include <pigpio.h>
-/* tell the compiler to use the module that has nanosleep */
-#include <time.h> 
-// so we can do stuff with strings
-#include<string.h> 
-// so we can print, for diagnosticsS
-#include <stdio.h>
-// so we can round stuff
-#include <math.h>
-#include <stdlib.h>     /* strtof */
+// COPRIGHT / LICENSE
+// parts of this code adapted from work by Guillermo A. Amaral B. <g@maral.me>, found on
+// https://elinux.org/RPi_GPIO_Code_Samples which is licenced under a CC BY-SA 3.0 license
+// (See https://creativecommons.org/licenses/by-sa/3.0/)
 
-int gpioSetMode(unsigned, unsigned); 
-int gpioInitialise();
-int gpioWrite(unsigned, unsigned);
-void gpioTerminate();
+// The first part of this code  (code from this point foward and before noted otherwise)
+// is covered by the license of this repository:
+// https://raw.githubusercontent.com/smouldery/shock-collar-control/master/LICENSE
+
+
+#include <time.h> // nanosleep ()
+#include <string.h> // so we can do stuff with strings
+#include <stdio.h> // so we can print, for diagnostics
+#include <math.h> // so we can round stuff
+#include <stdlib.h>  //for strtof
+
+#include <sys/stat.h> // for sysfs GPIO access
+#include <sys/types.h> // for sysfs GPIO access
+#include <fcntl.h> // for sysfs GPIO access
+#include <unistd.h> // for sysfs GPIO access
+
+
+#define IN   0 // for sysfs interface usage
+#define OUT  1 // for sysfs interface usage
+
+#define LOW  0 // for sysfs interface usage
+#define HIGH 1 // for sysfs interface usage
+
+#define PIN  24 // P1-18
+#define POUT 17  // Pi2b-11 // for sysfs interface usage
+
+// for sysfs interface usage
+static int GPIOExport(int pin);
+static int GPIOUnexport(int pin);
+static int GPIODirection(int pin, int dir);
+static int GPIORead(int pin);
+static int GPIOWrite(int pin, int value);
 
 
 int main(int argc, const char **argv) {
@@ -28,30 +49,31 @@ int main(int argc, const char **argv) {
     //printf(strof(shocktimesec));
     printf("\n test\n");
 
-    /*loads pgpio module*/
-    gpioInitialise();
-
     float shocktimesec = strtod(argv[2], NULL);
     // printf('%f', shocktimesec);
     // number of ticks in this time
     // clock_t shocktimeticks = rint(shocktimesecs * CLOCKS_PER_SEC);
 
-    
+    if (-1 == GPIOExport(POUT)) // Enable GPIO pin
+	    return(1);
 
+    if (-1 == GPIODirection(POUT, OUT)) // Set GPIO direction
+	    return(2);
 
-    gpioSetMode(17, 1); // Set GPIO27 as output.
-
-    gpioWrite(17, 0); // set gpio to zero, as a formality / just in case it was high. 
+	if (-1 == GPIOWrite(POUT, 0)) // Write GPIO value
+	    return(3);
 
     // function to transmit a 'zero' bit
     void zero() {
 
-        gpioWrite(17,1); // set pin to one to START transmitting the bit
+        if (-1 == GPIOWrite(POUT, 1)) // Write GPIO value
+	        return(3); // set pin to one to START transmitting the bit
 
     // wait 0 sec and 150000 nanosec
         nanosleep((const struct timespec[]){{0, 180000L}}, NULL);
         
-        gpioWrite(17, 0); // set pin to zero to STOP transmitting the bit. 
+        if (-1 == GPIOWrite(POUT, 0)) // Write GPIO value
+	        return(3); // set pin to zero to STOP transmitting the bit. 
 
     //  wait 0 sec and 650000 nanosec */
         nanosleep((const struct timespec[]){{0, 600000L}}, NULL);
@@ -60,12 +82,14 @@ int main(int argc, const char **argv) {
 
     // function to transmit a 'one' bit
     void one() {
-        gpioWrite(17,1); // set pin to one to START transmitting the bit
+        if (-1 == GPIOWrite(POUT, 1)) // Write GPIO value
+	        return(3); // set pin to one to START transmitting the bit
 
         /* wait 0 sec and 730000 nanosec */
         nanosleep((const struct timespec[]){{0, 730000L}}, NULL);
         
-        gpioWrite(17,0); // set pin to zero to STOP transmitting the bit.
+        if (-1 == GPIOWrite(POUT, 0)) // Write GPIO value
+	        return(3); // set pin to zero to STOP transmitting the bit.
         
         // wait 0 sec and 70000 nanosec
         nanosleep((const struct timespec[]){{0, 140000L}}, NULL);
@@ -94,10 +118,12 @@ int main(int argc, const char **argv) {
 
     do {
         // transmit the START bit. same each time. 
-        gpioWrite(17,1); // set pin to one to START transmitting the bit
+        if (-1 == GPIOWrite(POUT, 1)) // Write GPIO value
+	        return(3); // set pin to one to START transmitting the bit
         /* wait 0 sec and 1300000 nanosec */
         nanosleep((const struct timespec[]){{0, 1500000L}}, NULL);
-        gpioWrite(17,0); // set pin to zero to STOP transmitting the bit.
+        if (-1 == GPIOWrite(POUT, 0)) // Write GPIO value
+	        return(3); // set pin to zero to STOP transmitting the bit.
         // wait 0 sec and 70000 nanosec
         nanosleep((const struct timespec[]){{0, 730000L}}, NULL);
 
@@ -127,14 +153,130 @@ int main(int argc, const char **argv) {
         printf("end time: %f", end_time_value);
         
 
-
-
         } while (current_time_value < end_time_value);
     // set gpio 17 to zero to be safe
-    gpioWrite(17, 0);
+    if (-1 == GPIOWrite(POUT, 0)) // Write GPIO value
+	    return(3);
 
+    if ( -1 == GPIOUnexport(PIN)) // Disable GPIO pin
+	    return(4);
 
-    //ends pigpio module
-    gpioTerminate();
+    return(0);
+}
+// CONTENT BEYOND THIS POINT IS LICENSED UNDER CC BY-SA 3.0
+// adapted from work by Guillermo A. Amaral B. <g@maral.me>
+// the code below this notice is NOT covered by the license repository
+// and instead uses the CC BY-SA 3.0 License
 
+int
+GPIOExport(int pin)
+{
+#define BUFFER_MAX 3
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (-1 == fd) {
+	fprintf(stderr, "Failed to open export for writing!\n");
+	return(-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return(0);
+}
+
+int
+GPIOUnexport(int pin)
+{
+    char buffer[BUFFER_MAX];
+    ssize_t bytes_written;
+    int fd;
+
+    fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (-1 == fd) {
+	fprintf(stderr, "Failed to open unexport for writing!\n");
+	return(-1);
+    }
+
+    bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+    write(fd, buffer, bytes_written);
+    close(fd);
+    return(0);
+}
+
+int
+GPIODirection(int pin, int dir)
+{
+    static const char s_directions_str[]  = "in\0out";
+
+#define DIRECTION_MAX 35
+    char path[DIRECTION_MAX];
+    int fd;
+
+    snprintf(path, DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+	fprintf(stderr, "Failed to open gpio direction for writing!\n");
+	return(-1);
+    }
+
+    if (-1 == write(fd, &s_directions_str[IN == dir ? 0 : 3], IN == dir ? 2 : 3)) {
+	fprintf(stderr, "Failed to set direction!\n");
+	return(-1);
+    }
+
+    close(fd);
+    return(0);
+}
+
+int
+GPIORead(int pin)
+{
+#define VALUE_MAX 30
+    char path[VALUE_MAX];
+    char value_str[3];
+    int fd;
+
+    snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_RDONLY);
+    if (-1 == fd) {
+	fprintf(stderr, "Failed to open gpio value for reading!\n");
+	return(-1);
+    }
+
+    if (-1 == read(fd, value_str, 3)) {
+	fprintf(stderr, "Failed to read value!\n");
+	return(-1);
+    }
+
+    close(fd);
+
+    return(atoi(value_str));
+}
+
+int
+GPIOWrite(int pin, int value)
+{
+    static const char s_values_str[] = "01";
+
+    char path[VALUE_MAX];
+    int fd;
+
+    snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+    fd = open(path, O_WRONLY);
+    if (-1 == fd) {
+	fprintf(stderr, "Failed to open gpio value for writing!\n");
+	return(-1);
+    }
+
+    if (1 != write(fd, &s_values_str[LOW == value ? 0 : 1], 1)) {
+	fprintf(stderr, "Failed to write value!\n");
+	return(-1);
+    }
+
+    close(fd);
+    return(0);
 }
